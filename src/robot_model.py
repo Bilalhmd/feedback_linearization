ed9e85db121754cf5d0b864ea761f1eea4ae2391
Bilal Hammoud <bhammoud@tuebingen.mpg.de>
2017-12-01 16:40:41 -0500

    added basic python files

---
+++ src/robot_model.py
@@ -0,0 +1,24 @@
+import numpy as np 
+
+
+class RobotModel(object):
+
+    def __init__(self):
+        pass
+
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+    def _create_mass_matrix(self):
+        pass
+
+    def _create_coriolis_matrix(self):
+        pass
+
+    def _create_gravity_matrix(self):
+        pass
+
+    
+
